15eeb573da89ae2d7e34585b399a74e2c9fad25b
jai <solitaire@tygger.net>
2016-09-07 22:27:16 +1000

    Add lunch list

--- plugins/lunch.py
+++ plugins/lunch.py
@@ -1,15 +1,8 @@
 from plugins.base import PluginResponse, Plugin
-from random import randint
 import sys
 
-class jibo(Plugin):
-	def __init__(self):
-		self.keyword = "jibo"
-		
-	def command(self, text):
-		response = PluginResponse()
-		response.setText("Why don't you go fuck yourself")
-		return response
+from random import randint
+
 
 class lunch(Plugin):	
 
@@ -20,37 +13,45 @@ class lunch(Plugin):
 		)
 
 	lunches = (
-'the Korean place',
-'and the supermarket',
-'Bay city burrito',
-'Beer deluxe',
-'Haddons',
-'Hawthorn',
-'Kebabji',
-'Le resistance',
-'Lucy\'s Dumplings',
-'Samurai',
-'Santorini',
-'Schnitz',
-'shuji sushi',
-'Spud bar',
-'Subway',
-'The nevermind',
-'The roll place in the plaza', 
-'Zen'
+		'the Korean place',
+		'the supermarket',
+		'Bay city burrito',
+		'Beer deluxe',
+		'Haddons',
+		'Hawthorn',
+		'Kebabji',
+		'Le resistance',
+		'Lucy\'s Dumplings',
+		'Samurai',
+		'Santorini',
+		'Schnitz',
+		'shuji sushi',
+		'Spud bar',
+		'Subway',
+		'The nevermind',
+		'The roll place in the plaza', 
+		'Zen'
 		)
 
 	def __init__(self):
 		self.keyword = "lunch"
 
 	def command(self, text):
-		
+	
 		response = PluginResponse()
 		response.setText("The usual place")
 		
+
 		try:
 			if(self.keyword == text[0]):
-				response.setText(self.chooseLunch())   
+				if (len(text) == 2):
+					if (text[1] == 'list'):
+						resp = ''
+						for s in ["{}\n".format(x) for x in self.lunches]:
+							resp += s
+						response.setText( resp)
+				else:
+					response.setText(self.chooseLunch())   
 			else:
 				response.setText("I have no idea what that is")
 		except: 
--- plugtest.py
+++ plugtest.py
@@ -12,4 +12,6 @@ for c,k in COMMANDS.items():
 	response = k.command(c.split())
 	print(response.getText())
 
+response = COMMANDS['lunch'].command(("lunch", "list"))
+print(response.getText())
 
