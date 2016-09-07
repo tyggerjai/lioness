0c1c3c0da2536ada942bbb5ee41a42c02a4d7bc4
jai <solitaire@tygger.net>
2016-09-07 22:28:39 +1000

    add jibo plugin again

---
+++ plugins/jibo.py
@@ -0,0 +1,12 @@
+from plugins.base import PluginResponse, Plugin
+import sys
+
+
+class jibo(Plugin):
+	def __init__(self):
+		self.keyword = "jibo"
+		
+	def command(self, text):
+		response = PluginResponse()
+		response.setText("Why don't you go fuck yourself")
+		return response
