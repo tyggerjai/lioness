bfc28e427cc4765567df54d5bdbc0b66d3dd1026
tyggerjai <solitaire@lioness.tygger.net>
2017-06-15 10:05:28 +1000

    Added storing to db

---
+++ plugins/store.py
@@ -0,0 +1,33 @@
+########
+# Base URL storer
+######
+from plugins.base import PluginResponse, Plugin
+import sys
+import requests
+import html2text
+
+class store(Plugin):
+    def __init__(self, dbconn):
+        self.keyword = "store"
+        self.response = PluginResponse()
+        self.error = ""
+        self.dbconn = dbconn
+
+
+    def command(self, args):
+        self.response.setText("Nope")
+        text = args.text
+        stored = text[0]
+        user = args.user
+        tag = "generic"
+
+        if (len(text) > 1):
+            tag = text[1]
+        try:
+            self.error = self.dbconn.query("""INSERT INTO `store`(`text`, `tag`, `user`) VALUES(%s, %s, %s)""", (stored, tag, user))
+            self.response.setText("Added!")
+        except:
+            self.response.setText("Cannot la: {}".format(self.error))
+
+
+        return self.response
